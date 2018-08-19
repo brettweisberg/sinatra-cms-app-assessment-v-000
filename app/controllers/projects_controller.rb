01c01df070ea29c891dc1a6cc3a4dcbdc9a922bc
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-19 19:35:45 +0000

    automatically backed up by learn

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -35,7 +35,7 @@ class ProjectsController < ApplicationController
 
   post '/projects/:id' do #updates the entry based on the edit route
     @project = Tweet.find(params[:id])
-    if !params[:name].empty? && !params[:description].empty? && !params[:completion_date].empty? && logged_in? && current_user.id == @project.user_id
+    if !params[:name].empty? && !params[:description].empty? && !params[:completion_date].empty? && logged_in? && current_user.id == @project.client_id
         @project.update(name: params[:name], params[:description], params[:completion_date])
         redirect "/projects/#{@project.id}"
     else
